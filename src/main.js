985799225afce7963b98f939286b6f0ead973ff2
1110211037 <1110211037@gap.wzu.edu.tw>
2025-12-30 23:06:32 +0800

    end

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// 初始化 AOS 動畫
+AOS.init({
+  duration: 1000,
+  once: false,
+});
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // 如果需要，可以在這裡添加額外的 Vite 配置
+  // 但對於這個專案，默認配置就足夠了
+})
